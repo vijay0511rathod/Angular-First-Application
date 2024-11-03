a4460769e37c94d1680cda177bb1416b0b17b440
vijay Rathod <97028352+vijay0511rathod@users.noreply.github.com>
2024-11-03 06:40:23 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { DemoComponent } from './demo/demo.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,DemoComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'MarvellousFirst';
+  name: string = '';
+}
+
